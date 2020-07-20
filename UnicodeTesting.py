94ffafd89ed804ba93ccd8854232a53a3278125f
Mykel Brinkerhoff <mykelbrinkerhoff@Mykels-iMac.routerlogin.net>
2020-07-20 11:02:52 -0700

    Reading in a file and outputing a file

--- UnicodeTesting.py
+++ UnicodeTesting.py
@@ -3,18 +3,31 @@
 
 filename = 'test.txt'
 
+# Reading an entire file at once'
+with open(filename) as f_obj:
+    contents = f_obj.read()
+
+# Reading line by line
 # with open(filename) as f_obj:
 #     for line in f_obj:
 #         print(line.rstrip())
 
+# Opening the file one line at a time
 # with open(filename) as f_obj:
 #     lines = f_obj.readlines()
 
 # for line in lines:
 #     print(line.rstrip())
 
-squares = []
-for x in range(1,11):
-    squares.append(x**2)
+# Testing lists
+# squares = []
+# for x in range(1,11):
+#     squares.append(x**2)
 
-print(squares)
+# print(squares)
+
+# Writing to an output
+output = 'output.txt'
+
+with open(output, 'w') as f:
+    f.write(contents.rstrip())
