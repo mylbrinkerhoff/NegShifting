d2540736fb77ee1d3127431f4750e92589779456
Mykel Brinkerhoff <mykelbrinkerhoff@Mykels-iMac.routerlogin.net>
2020-07-20 10:49:35 -0700

    Added Unicode testing and fixed .gitignore for datasets

--- SearchNegatives.py
+++ SearchNegatives.py
@@ -9,18 +9,21 @@ import re
 
 negative_pattern = re.compile('ingenting')
 non_negative_pattern = re.compile('[^ingenting]')
+
+negative = []
+nonnegative = []
+
 for line in fi.input():
+    line = line.rstrip()
     if negative_pattern.search(line):
-    
-        negative = []
         negative.append(line)
-        print ('updated negative list: ', negative)
+        # print ('updated negative list: ', negative)
      #if non_negative_pattern.search(line):
     else:
-        nonnegative = []
         nonnegative.append(line)
-        print ('updated nonnegative list: ', nonnegative)
+        # print ('updated nonnegative list: ', nonnegative)
 
-
+print(negative)
+print(nonnegative)
      
      
---
+++ UnicodeTesting.py
@@ -0,0 +1,20 @@
+# alphabet = 'αβγδεζηθικλμνξοπρςστυφχψ'
+# print(alphabet)
+
+filename = 'test.txt'
+
+# with open(filename) as f_obj:
+#     for line in f_obj:
+#         print(line.rstrip())
+
+# with open(filename) as f_obj:
+#     lines = f_obj.readlines()
+
+# for line in lines:
+#     print(line.rstrip())
+
+squares = []
+for x in range(1,11):
+    squares.append(x**2)
+
+print(squares)
